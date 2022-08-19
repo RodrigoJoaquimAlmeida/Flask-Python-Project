43f7743e5038814d0062adb384a78a186e40d35d
Rodrigo Joaquim Almeida <rjgalmeida@gmail.com>
2022-08-19 14:48:40 -0300

    Routes.py updated

--- comunidadeflask/routes.py
+++ comunidadeflask/routes.py
@@ -32,7 +32,7 @@ def login():
     form_criarconta = FormCriarConta()
     if form_login.validate_on_submit() and 'botao_submit_fazerlogin' in request.form:
         usuario = Usuario.query.filter_by(email=form_login.email.data).first()
-        if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
+        if usuario and bcrypt.check_password_hash(usuario.senha.encode('utf-8'), form_login.senha.data):
             login_user(usuario, remember=form_login.lembrar_dados.data)
             flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
             par_next = request.args.get('next')
@@ -43,7 +43,7 @@ def login():
         else:
             flash(f'Falha no login. E-mail ou senha incorretos', 'alert-danger')
     if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
-        senha_crypt = bcrypt.generate_password_hash(form_criarconta.senha.data)
+        senha_crypt = bcrypt.generate_password_hash(form_criarconta.senha.data).decode('utf-8')
         usuario = Usuario(username=form_criarconta.username.data, email=form_criarconta.email.data, senha=senha_crypt)
         database.session.add(usuario)
         database.session.commit()
