c3cadb88b284a8fd098869a34d054033b2f09582
Rodrigo Joaquim Almeida <rjgalmeida@gmail.com>
2022-08-19 10:43:00 -0300

    commit inicial

---
+++ comunidadeflask/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'c09ede5df1bd3b1c377d385c6b2bd937'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projetoflask.db'
+
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'alert-info'
+
+from comunidadeflask import routes
+
---
+++ main.py
@@ -0,0 +1,4 @@
+from comunidadeflask import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
